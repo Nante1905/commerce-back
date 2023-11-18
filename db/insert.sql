9e885940cbb0ee2bb201a9fb1b4f4f207d645033
sarobidy13 <sarobidyfitahiana01@gmail.com>
2023-11-18 19:29:07 +0100

    data test

--- db/insert.sql
+++ db/insert.sql
@@ -1,10 +1,10 @@
+
 -- Pour la table categorie
 INSERT INTO categorie (reference, nom) VALUES
 ('C001', 'Cahier'),
 ('C002', 'Stylo'),
 ('C003', 'Papier'),
 ('C004', 'Ordinateur');
-
 -- Pour la table direction
 INSERT INTO direction (nom) VALUES
 ('Direction des Ressources Humaines'),
@@ -19,17 +19,35 @@ INSERT INTO demande (reference, jour, est_ouvert, id_direction) VALUES
 ('D003', '2023-03-03', false, 3),
 ('D004', '2023-04-04', true, 4);
 
+----
+INSERT INTO demande_proforma(reference,delai_livraison,jour_demande) VALUES 
+('P001', '2023-11-20', '2023-11-18'),
+('P002', '2023-11-25', '2023-11-20');
+
 -- Pour la table fournisseur
 INSERT INTO fournisseur (nom, reference, email, telephone) VALUES
-('Fournisseur A', 'F001', 'fournisseurA@email.com', '0123456789'),
-('Fournisseur B', 'F002', 'fournisseurB@email.com', '0987654321'),
-('Fournisseur C', 'F003', 'fournisseurC@email.com', '1234567890');
+('Leader', 'F001', 'leader@email.com', '0123456789'),
+('Shoprite', 'F002', 'shoprite@email.com', '0987654321'),
+('Massin', 'F003', 'massin@email.com', '1234567890');
+
+INSERT INTO demande_proforma_fournisseur (id_fournisseur, id_demande_proforma) VALUES 
+(1, 1),
+(2, 1),
+(1, 2),
+(3, 2);
 
 -- Pour la table mode_paiement
 INSERT INTO mode_paiement (nom) VALUES
 ('Carte de crédit'),
 ('Carte de débit'),
 ('Virement bancaire');
+
+-- Pour la table employe
+INSERT INTO employe (nom, prenom, date_naissance, date_embauche, email, mot_de_passe, id_direction)
+VALUES
+  ('Razanamahafaly', 'Andriambololona', '1990-01-01', '2020-01-01', 'andriambololona@email.com', 'mdp1', 1),
+  ('Rakotonirina', 'Fenoarivo', '1991-02-02', '2021-02-02', 'fenoarivo@email.com', 'mdp2', 2),
+  ('Ravelonirina', 'Harimalala', '1992-03-03', '2022-03-03', 'harimalala@email.com', 'mdp3', 3);
 
 -- Pour la table article
 INSERT INTO article (designation, reference, id_categorie) VALUES
@@ -50,7 +68,6 @@ INSERT INTO resultat_proforma (format_prix, delai_livraison, date_saisie, id_dem
 (200, '2023-02-02', '2023-02-02', 2),
 (300, '2023-03-03', '2023-03-03', 3);
 
--- Pour la table bon_commande
 INSERT INTO bon_commande (reference, date_creation, livraison_partielle, delai_livraison, status, id_mode_paiement, id_fournisseur) VALUES
 ('BC001', '2023-01-01', true, '2023-01-01', true, 1, 1),
 ('BC002', '2023-02-02', false, '2023-02-02', false, 2, 2),
@@ -66,7 +83,7 @@ INSERT INTO demande_details (id_article, id_demande, quantite, status) VALUES
 INSERT INTO demande_proforma_details (id_article, id_demande_proforma, quantite) VALUES
 (1, 1, 10),
 (2, 2, 20),
-(3, 3, 30);
+(3, 1, 30);
 
 -- Pour la table fournisseur_categorie
 INSERT INTO fournisseur_categorie (id_categorie, id_fournisseur) VALUES
@@ -90,36 +107,17 @@ INSERT INTO bon_commande_details (id_article, id_bon_commande, quantite, pu_ht, 
 INSERT INTO proforma_besoin (id_demande, id_proforma) VALUES
 (1, 1),
 (2, 2),
-(3, 3);
+(3, 1);
 
 -- Pour la table chef_direction
 INSERT INTO chef_direction (id_direction, id_employe, date_promotion) VALUES
 (1, 1, '2023-01-01'),
 (2, 2, '2023-02-02'),
-(3, 3, '2023-03-03'),
-(4, 4, '2023-04-04');
+(3, 3, '2023-03-03');
 
--- Pour la table employe
-INSERT INTO employe (nom, prenom, email, telephone, id_direction) VALUES
-('Dupont', 'Jean', 'jean.dupont@email.com', '0123456789', 1),
-('Martin', 'Paul', 'paul.martin@email.com', '0987654321', 2),
-('Leclerc', 'Marie', 'marie.leclerc@email.com', '1234567890', 3),
-('Durand', 'Sophie', 'sophie.durand@email.com', '1122334455', 4);
 
--- Pour la table employe_categorie
--- Pour la table employe_categorie
-INSERT INTO employe_categorie (id_categorie, id_employe) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4);
 
--- Pour la table employe_direction
-INSERT INTO employe_direction (id_employe, id_direction) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4);
+
 
 
 
